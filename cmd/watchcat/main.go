b4bb3026506c4b7521ca5262d56345ac0be336b2
kudohamu <kudohamu91086@gmail.com>
2017-09-24 07:00:34 +0900

    feat: update version to v0.5.0

--- cmd/watchcat/main.go
+++ cmd/watchcat/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "watchcat"
-	app.Version = "v0.4.0"
+	app.Version = "v0.5.0"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "conf",
